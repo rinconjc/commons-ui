8d9f83922a46b885277aac488fd7721eb9c99e14
Julio Rincon <jcrinconc@gmail.com>
2017-03-01 07:40:24 +1100

    fixes

--- src/commons_ui/core.cljs
+++ src/commons_ui/core.cljs
@@ -107,7 +107,7 @@
     (case type
       "text" [:input.form-control attrs]
       "password" [:input.form-control attrs]
-      "select" [:select.form-control (assoc attrs :default-value (or (:value attrs)  "")) children]
+      "select" [:select.form-control  children]
       "textarea" [:textarea.form-control attrs]
       "file" [:input attrs]
       "typeahead" [typeahead (assoc attrs :model model)]
@@ -157,7 +157,7 @@
 (defn alert-box [{:keys [type fade-after] :or {type "danger"}} text show?]
   (when @show?
     (if fade-after (js/setTimeout #(reset! show? false) (* 1000 fade-after)))
-    [:div.col-md-3 {:style {:z-index 101}}
+    [:div.col-md-3.text-center {:style {:z-index 101}}
      [:div.alert.alert-dismissible {:class (str "alert-" type)}
       [:button.close {:on-click #(reset! show? nil) :aria-label "Close"}
        [:span {:aria-hidden true} "×"]] text]]))
